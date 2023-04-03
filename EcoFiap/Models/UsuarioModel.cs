b170825446fd749558209c8e69e25fa98c880c31
André Sales <asalesg@gmail.com>
2023-04-03 19:36:26 -0300

    arrumei a usuariomodel pra combinar com usuariorepository seguindo a aula do cap 5

--- EcoFiap/Models/UsuarioModel.cs
+++ EcoFiap/Models/UsuarioModel.cs
@@ -3,20 +3,29 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EcoFiap.Models
 {
+    [Table("USUARIO")]
     public class UsuarioModel
     {
         [Key]
         [Column("USUARIOID")]
-        public int UsuarioId { get; set; }
+        public int UsuarioId { get; set; } //sulfixo Id exclui a NECESSIDADE da notação key porem uso pra ostentar
 
-        public string Nome { get; set; }
-        
-        public string Endereco { get; set; }
+        [Column("NOMEUSUARIO")]
+        [Required(ErrorMessage ="Nome é obrigatório!")]
+        public string? Nome { get; set; }
 
+        [Column("ENDERECOUSUARIO")]
+        [Required(ErrorMessage = "Endereço é obrigatório!")]
+        [StringLength(100, MinimumLength = 10, ErrorMessage ="O endereço deve ter, no minimo 10 e, no máximo, 100 caracteres")]
+        public string? Endereco { get; set; }
 
-        public string email { get; set; }
+        [Column("EMAILSUARIO")]
+        [Required(ErrorMessage = "Email é obrigatório!")]
+        public string? email { get; set; }
 
-        public string Telefone { get; set; }
+        [Column("TELEFONEUSUARIO")]
+        [Required(ErrorMessage = "Telefone é obrigatório!")]
+        public string? Telefone { get; set; }
 
 
     }
---
+++ EcoFiap/Repository/UsuarioRepository.cs
@@ -0,0 +1,70 @@
+﻿namespace EcoFiap.Repository.Context
+{
+    using Fiap.Web.AspNet.Models;
+    using Fiap.Web.AspNet.Repository.Context;
+
+    namespace Fiap.Web.AspNet.Repository
+    {
+        public class UsuarioRepository
+        {
+
+            private readonly DataBaseContext dataBaseContext;
+
+            public UsuarioRepository(DataBaseContext ctx)
+            {
+                dataBaseContext = ctx;
+            }
+
+
+
+            public IList<UsuarioModel> Listar()
+            {
+                var lista = new List<UsuarioModel>();
+
+                // Efetuando a listagem (Substituindo o Select *)
+                lista = dataBaseContext.Usuario.ToList<UsuarioModel>();
+
+                return lista;
+            }
+
+            public UsuarioModel Consultar(int id)
+            {
+                // Recuperando o objeto Usuario de um determinado Id
+                var Usuario = dataBaseContext.Usuario.Find(id);
+
+                return Usuario;
+            }
+
+            public void Inserir(UsuarioModel Usuario)
+            {
+                // Adiciona o objeto preenchido pelo usuário
+                dataBaseContext.Usuario.Add(Usuario);
+
+                // Salva as alterações
+                dataBaseContext.SaveChanges();
+
+            }
+
+            public void Alterar(UsuarioModel Usuario)
+            {
+                dataBaseContext.Usuario.Update(Usuario);
+
+                // Salva as alterações
+                dataBaseContext.SaveChanges();
+            }
+
+            public void Excluir(int id)
+            {
+                var Usuario = new UsuarioModel(id, "");
+
+                dataBaseContext.Usuario.Remove(Usuario);
+
+                // Salva as alterações
+                dataBaseContext.SaveChanges();
+
+            }
+
+
+        }
+    }
+}
